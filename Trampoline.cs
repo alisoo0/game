238154b98b86c0038713ceb80cc2c5403a49d567
alisoo0 <alisa.rodionova0112@gmail.com>
2025-01-05 15:23:37 +0600

    Update Trampoline.cs

--- Trampoline.cs
+++ Trampoline.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Trampoline : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    void OnTriggerEnter(Collider other)
     {
-        
+        other.GetComponent.<Jump>().jumpStrenght = 10;
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    void OnTriggerEnter(Collider other)
     {
-        
+        other.GetComponent.<Jump>().jumpStrenght = 2;
     }
 }
